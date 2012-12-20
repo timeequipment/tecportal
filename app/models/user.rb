5f48225fc01bd8cd8a5ee69fad2d2f65aa7005bf
Lane Buckingham <lanebuckingham@gmail.com>
2012-12-20 13:41:26 -0500

    Login with user name instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,39 @@
 class User
   include Mongoid::Document
+
+  # --------------------------------------------------------------------
+  # Regarding 'Forgot your password'
+  #
+  #   If you want to utilize 'Forgot your password' links, 
+  #   then add :recoverable to the list below, and uncomment the  
+  #   Recoverable section, then run rake db:migrate.
+  #   Also, you'll have to ensure that ActionMailer is setup and working, 
+  #   which I've set it up in the app to use gmail.com.  You'll have to 
+  #   identify an email service and an email address which will send the 
+  #   'Forgot your password' emails to the users.  
+  #
+  #   See:  config/environment/development.rb for email setup
+  #
+  #---------------------------------------------------------------------
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :authentication_keys => [:username] 
 
   ## Database authenticatable
+  field :username,           :type => String, :default => ""
   field :email,              :type => String, :default => ""
   field :encrypted_password, :type => String, :default => ""
 
+  validates_presence_of :username
   validates_presence_of :email
   validates_presence_of :encrypted_password
   
-  ## Recoverable
-  field :reset_password_token,   :type => String
-  field :reset_password_sent_at, :type => Time
+  ## Recoverable 
+  # field :reset_password_token,   :type => String
+  # field :reset_password_sent_at, :type => Time
 
   ## Rememberable
   field :remember_created_at, :type => Time
@@ -40,5 +58,10 @@ class User
 
   ## Token authenticatable
   # field :authentication_token, :type => String
+
+  def email_required?
+    false
+  end
 end
+
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,19 @@ Newauth::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+
+  #Missing host to link to! Please provide the :host parameter, set default_url_options[:host], or set :only_path to true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
