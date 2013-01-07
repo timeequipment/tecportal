6105be2ecebe0d5e9d54434b7c08a4d9107477cc
Lane Buckingham <lanebuckingham@gmail.com>
2013-01-07 14:36:19 -0500

    Clean up

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Newauth::Application.initialize!
 
-# force Rails into production mode when                          
-# you don't control web/app server and can't set it the proper way                  
-ENV['RAILS_ENV'] ||= 'production'
