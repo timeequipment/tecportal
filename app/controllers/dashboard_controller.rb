6b1a22ddf7491ef613a7f885d0cdc030754120f2
Lane Buckingham <lanebuckingham@gmail.com>
2013-11-19 13:27:06 -0500

    DateTime edits

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,20 @@
 class DashboardController < ApplicationController
 	before_filter :authenticate_user!
 
-  def index
-  	a = Time.now.hour
-  	if a < 12 
-  		@daypart = "morning"
-  	elsif a < 18
-  		@daypart = "afternoon"
-  	else 
-  		@daypart = "evening"
-  	end
-  end
+	def index
+
+		# Get PST local time
+		zone = ActiveSupport::TimeZone[-8]	
+		psttime = DateTime.now.in_time_zone(zone)
+		psthour = psttime.hour
+
+		# Show 'morning', 'afternoon', or 'evening' based on PST
+		if psthour < 12 
+			@daypart = "morning"
+		elsif psthour < 18
+			@daypart = "afternoon"
+		else 
+			@daypart = "evening"
+		end
+	end
 end
