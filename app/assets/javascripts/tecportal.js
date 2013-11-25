7e62588602be3fa65db51260780c54531b12c365
Lane Buckingham <lanebuckingham@gmail.com>
2013-11-25 06:14:39 -0500

    Precompiled assets

--- app/assets/javascripts/tecportal.js
+++ app/assets/javascripts/tecportal.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 
-	// Style all select elements with the Chosen jQuery plugin
-	$("select").chosen();
+	// // Style all select elements with the Chosen jQuery plugin
+	// $("select").chosen();
 });
