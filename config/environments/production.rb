445c71cc4c9e82c74cf34a0a6691e1c1a619df06
Lane Buckingham <lanebuckingham@gmail.com>
2014-02-11 22:29:33 -0500

    DJ fix 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Tecportal::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Don't buffer stdout (for foreman logging)
+  $stdout.sync = true
+
   # Reload code between requests
   config.cache_classes = false
 
