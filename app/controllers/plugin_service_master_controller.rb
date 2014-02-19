da32f9cb9ed90f132004a950331c8df3e881bbd1
Lane Buckingham <lanebuckingham@gmail.com>
2014-02-19 14:37:29 -0500

    Created plugin

---
+++ app/controllers/plugin_service_master_controller.rb
@@ -0,0 +1,28 @@
+class PluginServiceMasterController < ApplicationController
+  def index
+  end
+
+  def settings
+  end
+
+  def load_scheds
+  end
+
+  def save_scheds
+  end
+
+  def filter
+  end
+
+  def next_week
+  end
+
+  def prev_week
+  end
+
+  def load_customer
+  end
+
+  def save_customer
+  end
+end
